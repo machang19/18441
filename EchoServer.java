c56ac13ac4616395a097b8ce5d1a20c17d39b1e2
Michael Chang <machang@andrew.cmu.edu>
2018-01-30 14:50:05 -0500

    server sends text files

--- EchoServer.java
+++ EchoServer.java
@@ -4,52 +4,55 @@ import java.io.*;
 public class EchoServer 
 { 
  public static void main(String[] args) throws IOException 
-   { 
-    ServerSocket serverSocket = null; 
+   {
+    ServerSocket serverSocket = null;
 
-    try { 
-         serverSocket = new ServerSocket(10007); 
-        } 
-    catch (IOException e) 
-        { 
-         System.err.println("Could not listen on port: 10007."); 
-         System.exit(1); 
-        } 
+    try {
+         serverSocket = new ServerSocket(8000);
+        }
+    catch (IOException e)
+        {
+         System.err.println("Could not listen on port: 10007.");
+         System.exit(1);
+        }
+    Socket clientSocket = null;
+    System.out.println("Waiting for connection.....");
 
-    Socket clientSocket = null; 
-    System.out.println ("Waiting for connection.....");
+    try {
+        clientSocket = serverSocket.accept();
+    } catch (IOException e) {
+        System.err.println("Accept failed.");
+        System.exit(1);
+    }
 
-    try { 
-         clientSocket = serverSocket.accept(); 
-        } 
-    catch (IOException e) 
-        { 
-         System.err.println("Accept failed."); 
-         System.exit(1); 
-        } 
+    System.out.println("Connection successful");
+    System.out.println("Waiting for input.....");
 
-    System.out.println ("Connection successful");
-    System.out.println ("Waiting for input.....");
+    DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+    BufferedReader in = new BufferedReader(
+            new InputStreamReader(clientSocket.getInputStream()));
 
-    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), 
-                                      true); 
-    BufferedReader in = new BufferedReader( 
-            new InputStreamReader( clientSocket.getInputStream())); 
 
-    String inputLine; 
 
-    while ((inputLine = in.readLine()) != null) 
-        { 
-         System.out.println ("Server: " + inputLine); 
-         out.println(inputLine); 
+    File testFile = new File("C:\\Users\\Michael Chang\\18441\\test.txt");
+    BufferedInputStream bis = null;
+    OutputStream os = null;
+    byte[] mybytearray = new byte[(int) testFile.length()];
+    FileInputStream fis = new FileInputStream(testFile);
+    bis = new BufferedInputStream(fis);
+    bis.read(mybytearray, 0, mybytearray.length);
+    os = clientSocket.getOutputStream();
+    System.out.println("Sending " + testFile + "(" + mybytearray.length + " bytes)");
+    out.writeBytes("HTTP/1.1 200 OK\r\n");
+    out.writeBytes("Content-Type: text/html\r\n\r\n");
+    os.write(mybytearray, 0, mybytearray.length);
+    System.out.println("Done.");
 
-         if (inputLine.equals("Bye.")) 
-             break; 
-        } 
 
-    out.close(); 
-    in.close(); 
-    clientSocket.close(); 
-    serverSocket.close(); 
+
+    //out.close();
+    //in.close();
+    //clientSocket.close();
+    //serverSocket.close();
    } 
 } 
